ac1a2105986d16b1e5d8c1a7bad5828ed49f7a79
Luciano Ramello <luchosr@gmail.com>
2024-08-21 14:18:04 +0200

    feat: Adds quantity items handlers

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,66 @@ import { useState } from 'react';
 import Guitar from './components/Guitar';
 import Header from './components/Header';
 import { db } from './data/db';
-import { useEffect } from 'react';
 
 const App = () => {
   const [data, setData] = useState(db);
+  const [cart, setCart] = useState([]);
 
+  const MAX_ITEMS = 5;
+  const MIN_ITEMS = 1;
+
+  const addToCart = (item) => {
+    const itemaExists = cart.findIndex((guitar) => guitar.id === item.id);
+    if (itemaExists >= 0) {
+      const updatedCart = [...cart];
+      updatedCart[itemaExists].quantity++;
+      setCart(updatedCart);
+    } else {
+      item.quantity = 1;
+      setCart([...cart, item]);
+    }
+  };
+
+  function removeFromCart(id) {
+    setCart((prevCart) => prevCart.filter((guitar) => guitar.id !== id));
+  }
+
+  function increaseQuantity(id) {
+    const updatedCart = cart.map((item) => {
+      if (item.id === id && item.quantity < MAX_ITEMS) {
+        return { ...item, quantity: item.quantity + 1 };
+      }
+      return item;
+    });
+    setCart(updatedCart);
+  }
+
+  function decreaseQuantity(id) {
+    const updatedCart = cart.map((item) => {
+      if (item.id === id && item.quantity > MIN_ITEMS) {
+        return { ...item, quantity: item.quantity - 1 };
+      }
+      return item;
+    });
+    setCart(updatedCart);
+  }
   return (
     <>
-      <Header />
+      <Header
+        cart={cart}
+        removeFromCart={removeFromCart}
+        increaseQuantity={increaseQuantity}
+        decreaseQuantity={decreaseQuantity}
+      />
 
       <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
 
-        <div className="row mt-5">{/* <Guitar price={299} /> */}</div>
+        <div className="row mt-5">
+          {data.map((guitar) => (
+            <Guitar key={guitar.id} guitar={guitar} addToCart={addToCart} />
+          ))}
+        </div>
       </main>
 
       <footer className="bg-dark mt-5 py-5">
